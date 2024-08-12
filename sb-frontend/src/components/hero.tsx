37d6944589b10344b02baf9284c4e4a358b7949d
Alex Christakou <alexchristakou@gmail.com>
2024-08-12 12:24:23 +0100

    fix: add correct links to hero buttons

--- sb-frontend/src/components/hero.tsx
+++ sb-frontend/src/components/hero.tsx
@@ -1,8 +1,8 @@
-
 import { MoveRight, Github } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import HeroImage from "../../public/hero-img/hero-image-5.jpeg";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -14,33 +14,32 @@ export default function Hero() {
         quality={50}
         className="-z-[1] object-cover m-0"
         fill={true}
-        
       />
-      
+
       <div className="container mx-auto">
         <div className="grid grid-cols-1 gap-8 items-center lg:grid-cols-1">
-              <div className="flex gap-4 flex-col backdrop-brightness-75 p-10">
-                  <div className="flex gap-4 flex-col">
-                                    <h1 className="text-5xl md:text-7xl max-w-lg text-left font-regular text-primary-foreground">
-                                        Welcome to award winning banking
-                    </h1>
-                        <p className="text-md leading-relaxed tracking-tight text-primary-foreground max-w-md text-left">
-                          See why we're trusted by over a million customers across Europe.*
-                    </p>
-                  </div>
-                  <div className="flex flex-row gap-2">
-                        <Button  className="gap-2 p-4">
-                            Create Account
-                            <MoveRight className="w-4 h-4" />
-                    </Button>
-                        <Button
-                          className="gap-2 p-4" variant="outline">
-                            Sign-in
-                    </Button>
-                  </div>
-              </div>
+          <div className="flex gap-4 flex-col backdrop-brightness-75 p-10">
+            <div className="flex gap-4 flex-col">
+              <h1 className="text-5xl md:text-7xl max-w-lg text-left font-regular text-primary-foreground">
+                Welcome to award winning banking
+              </h1>
+              <p className="text-md leading-relaxed tracking-tight text-primary-foreground max-w-md text-left">
+                See why we're trusted by over a million customers across
+                Europe.*
+              </p>
             </div>
+            <div className="block gap-2 sm:block ">
+              <Button className="gap-2 p-4 mr-2">
+                <Link href="/signup">Sign Up</Link>
+                <MoveRight className="w-4 h-4 xsm:hidden" />
+              </Button>
+              <Button className="gap-2 p-4" variant="outline">
+                <Link href="/login">Login</Link>
+              </Button>
+            </div>
+          </div>
         </div>
-        </div>
-        )
-};
+      </div>
+    </div>
+  );
+}
