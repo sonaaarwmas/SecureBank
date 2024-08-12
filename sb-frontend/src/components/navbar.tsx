8405c8da2135b7b09e163a9ff15a0475859b2482
Alex Christakou <alexchristakou@gmail.com>
2024-08-12 12:26:14 +0100

    fix: improve responsiveness and looks of navbar

--- sb-frontend/src/components/navbar.tsx
+++ sb-frontend/src/components/navbar.tsx
@@ -2,7 +2,7 @@ import { NavContent } from "./nav-content";
 
 export function DesktopNav() {
   return (
-      <nav className="flex gap-2 py-4 px-4 justify-center">
+      <nav className="flex gap-4 py-4 px-6 justify-center border-b-2 border-slate-50">
           <NavContent />
       </nav>
   )
