7e27e82e0c22e00054c06f48848dd0cc1ecf948d
Alex Christakou <alexchristakou@gmail.com>
2024-08-14 11:49:53 +0100

    fix: hydration problems on conditional rendering navbar

--- sb-frontend/src/components/navbar.tsx
+++ sb-frontend/src/components/navbar.tsx
@@ -17,7 +17,8 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation"; 
 
 interface RouteProps {
   href: string;
@@ -52,113 +53,151 @@ const authenticatedRoutes: RouteProps[] = [
     href: "/transactions",
     label: "Transactions",
   },
-  // {
-  //   href: "#",
-  //   label: "Sign Out",
-  // },
 ];
 
 export function NavBar() {
-  const[isOpen, setIsOpen] = useState<boolean>(false)
-  const { isAuthenticated, username, cash, logout } = useAuth();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { isAuthenticated, username, cash, logout, refreshAuthState } = useAuth();
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
   const routes = isAuthenticated ? authenticatedRoutes : unathenticatedRoutes;
+  const router = useRouter(); 
+
+  const handleLogout = () => {
+    logout();
+    router.push("/"); 
+  };
+
+  useEffect(() => {
+    console.log("NavBar re-rendered, refreshing auth state");
+    refreshAuthState();
+    setHasMounted(true);
+  }, [refreshAuthState]);
+
+  if (!hasMounted) {
+    // Render a minimal version of the NavBar on the server
+    return (
+      <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
+        <NavigationMenu className="mx-auto">
+          <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
+            <NavigationMenuItem className="flex gap-4 max-w-48 sm:max-w-36">
+              <a
+                rel="noreferrer noopener"
+                href="/"
+                className="ml-2 font-bold text-xl flex"
+              >
+                <Image
+                  src={Logo}
+                  alt="logo"
+                  sizes="100vw"
+                  style={{
+                    width: "auto",
+                    height: "auto",
+                  }}
+                />
+              </a>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
+    );
+  }
 
   return (
     <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
-    <NavigationMenu className="mx-auto">
-      <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
-        <NavigationMenuItem className="flex gap-4 max-w-48 sm:max-w-36">
-          <a
-            rel="noreferrer noopener"
-            href="/"
-            className="ml-2 font-bold text-xl flex"
-          >
-            <Image
-              src={Logo}
-              alt="logo"
-              sizes="100vw"
-              style={{
-                width: "auto",
-                height: "auto",
-              }}
-            />
-          </a>
-        </NavigationMenuItem>
-
-        {/* mobile */}
-        <span className="flex md:hidden">
-          <Sheet
-            open={isOpen}
-            onOpenChange={setIsOpen}
-          >
-            <SheetTrigger className="px-2">
-              <Menu
-                className="flex md:hidden h-5 w-5 mr-10"
-                onClick={() => setIsOpen(true)}
-              >
-                <span className="sr-only">Menu Icon</span>
-              </Menu>
-            </SheetTrigger>
-
-            <SheetContent side={"left"}>
-              <SheetHeader>
-                <SheetTitle className="font-bold text-xl">
-                  Fortress
-                </SheetTitle>
-              </SheetHeader>
-              <nav className="flex flex-col justify-center items-center gap-2 mt-4">
-                {routes.map(({ href, label }: RouteProps) => (
-                  <Link
-                    key={label}
-                    href={href}
-                    onClick={() => setIsOpen(false)}
-                    className={buttonVariants({ variant: "ghost" })}
-                  >
-                    {label}
-                  </Link>
-                ))}
-                {isAuthenticated && (
-                  <div className="flex flex-col justify-center items-center gap-2 mt-4">
-                    <span className="text-sm">
-                      {username} with ${cash}
-                    </span>
-                    <button onClick={logout} className=" bg-blue-500 px-2 py-2 rounded text-sm">
-                      Logout
-                    </button>
-                  </div>
-                )}
-              </nav>
-            </SheetContent>
-          </Sheet>
-        </span>
-
-        {/* desktop */}
-        <nav className="hidden md:flex gap-2">
-          {routes.map((route: RouteProps, i) => (
-            <Link
-              key={i}
-              href={route.href}
-              className={`text-[17px] ${buttonVariants({
-                variant: "ghost",
-              })}`}
+      <NavigationMenu className="mx-auto">
+        <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
+          <NavigationMenuItem className="flex gap-4 max-w-48 sm:max-w-36">
+            <a
+              rel="noreferrer noopener"
+              href="/"
+              className="ml-2 font-bold text-xl flex"
             >
-              {route.label}
-            </Link>
-          ))}
-        </nav>
+              <Image
+                src={Logo}
+                alt="logo"
+                sizes="100vw"
+                style={{
+                  width: "auto",
+                  height: "auto",
+                }}
+              />
+            </a>
+          </NavigationMenuItem>
 
-        {isAuthenticated && (
-          <div className="hidden md:flex justify-between gap-2 ">
-            <span className="text-sm text-center flex">
-              {username} with ${cash}
-            </span>
-            <button onClick={logout} className="text-[17px] bg-blue-500 text-white px-4 py-2 rounded">
-              Logout
-            </button>
-          </div>
-        )}
-      </NavigationMenuList>
-    </NavigationMenu>
-  </header>
+          {/* mobile */}
+          <span className="flex md:hidden">
+            <Sheet
+              open={isOpen}
+              onOpenChange={setIsOpen}
+            >
+              <SheetTrigger className="px-2">
+                <Menu
+                  className="flex md:hidden h-5 w-5 mr-10"
+                  onClick={() => setIsOpen(true)}
+                >
+                  <span className="sr-only">Menu Icon</span>
+                </Menu>
+              </SheetTrigger>
+
+              <SheetContent side={"left"}>
+                <SheetHeader>
+                  <SheetTitle className="font-bold text-xl">
+                    Fortress
+                  </SheetTitle>
+                </SheetHeader>
+                <nav className="flex flex-col justify-center items-center gap-2 mt-4">
+                  {routes.map(({ href, label }: RouteProps) => (
+                    <Link
+                      key={label}
+                      href={href}
+                      onClick={() => setIsOpen(false)}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {label}
+                    </Link>
+                  ))}
+                  {isAuthenticated && (
+                    <div className="flex flex-col justify-center items-center gap-2 mt-4">
+                      <span className="text-sm">
+                        {username} with ${cash}
+                      </span>
+                      <button onClick={handleLogout} className=" bg-blue-500 px-2 py-2 rounded text-sm">
+                        Logout
+                      </button>
+                    </div>
+                  )}
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </span>
+
+          {/* desktop */}
+          <nav className="hidden md:flex gap-2">
+            {routes.map((route: RouteProps, i) => (
+              <Link
+                key={i}
+                href={route.href}
+                className={`text-[17px] ${buttonVariants({
+                  variant: "ghost",
+                })}`}
+              >
+                {route.label}
+              </Link>
+            ))}
+          </nav>
+
+          {isAuthenticated && (
+            <div className="hidden md:flex justify-between gap-2 ">
+              <span className="text-sm text-center flex">
+                {username} with ${cash}
+              </span>
+              <button onClick={handleLogout} className="text-[17px] bg-blue-500 text-white px-4 py-2 rounded">
+                Logout
+              </button>
+            </div>
+          )}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
   );
 }
