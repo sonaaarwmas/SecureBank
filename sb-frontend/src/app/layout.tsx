9641f397baa2870e95828381c144036b335734f1
Alex Christakou <alexchristakou@gmail.com>
2024-08-15 16:20:41 +0100

    chore: remove unused components and tidy up the code

--- sb-frontend/src/app/layout.tsx
+++ sb-frontend/src/app/layout.tsx
@@ -1,29 +1,29 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 import { NavBar } from "@/components/navbar";
 import Footer from "@/components/footer";
 import { AuthProvider } from "../../context/AuthContext";
 
 export const metadata: Metadata = {
   title: "SecureBank",
-  description: "SecureBank is an open source project in .NET core with some security flaws, its purpose is to learn how to write good code from the bad practices found during penetration testing.",
+  description:
+    "SecureBank is an open source project in .NET core with some security flaws, its purpose is to learn how to write good code from the bad practices found during penetration testing.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <AuthProvider>
       <html lang="en">
-      <body>
-            <NavBar/> 
+        <body>
+          <NavBar />
           {children}
-          <Footer/>
-          </body>
+          <Footer />
+        </body>
       </html>
-      </AuthProvider>
+    </AuthProvider>
   );
 }
