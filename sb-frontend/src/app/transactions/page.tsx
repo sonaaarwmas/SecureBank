c47e3f1e9468616cd6d5a5c6f467a05d6da68cb8
Alex Christakou <alexchristakou@gmail.com>
2024-08-27 17:36:38 +0100

    chore: remove redundant authcheck component - auth controlled by global context

--- sb-frontend/src/app/transactions/page.tsx
+++ sb-frontend/src/app/transactions/page.tsx
@@ -1,10 +1,7 @@
-import AuthCheck from '../../components/authcheck';
 import Transactions from '../../components/transactions';
 const TransactionsPage: React.FC = () => {
   return (
-    // <AuthCheck> - removing auth check for now 
       <Transactions />
-    // </AuthCheck>
   );
 };
 export default TransactionsPage;
